93753288da475f75e6d19dfd3a356add9e2aae6d
Your Name <67040@pspu.ru>
2023-09-30 10:12:16 +0500

    first commit

---
+++ bin/newproj.dart
@@ -0,0 +1,5 @@
+import 'package:newproj/newproj.dart' as newproj;
+
+void main(List<String> arguments) {
+  print('Hello world: ${newproj.calculate()}!');
+}
---
+++ test/newproj_test.dart
@@ -0,0 +1,8 @@
+import 'package:newproj/newproj.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
